1178e5a6da947b7abbaf0ccd10a68d9d5676bc21
Ricardo Gauto <ricardogauto11@gmail.com>
2020-10-08 18:03:55 -0300

    Promesas y su tipado en TS

---
+++ lecciones/promesas2.ts
@@ -0,0 +1,18 @@
+// Promesas y tipado en Typescript
+
+const retirarDinero = (montoRetirar: number): Promise<number> => {
+    let dineroActual = 1000
+
+    return new Promise((resolve, reject) => {
+        if (montoRetirar > dineroActual) {
+            reject('No hay suficientes fondos')
+        } else {
+            dineroActual -= montoRetirar
+            resolve(dineroActual)
+        }
+    })
+}
+
+retirarDinero(1500)
+    .then(montoActual => console.log(`Me queda ${montoActual}`))
+    .catch(err => console.warn(err))
